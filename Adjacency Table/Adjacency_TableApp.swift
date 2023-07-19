a73de421e32cab684553cc5469dd4494248663a9
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2023-07-19 13:11:05 -0700

    Initial Commit

---
+++ Adjacency Table/Adjacency_TableApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adjacency_TableApp.swift
+//  Adjacency Table
+//
+//  Created by Dylan Conklin on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Adjacency_TableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
