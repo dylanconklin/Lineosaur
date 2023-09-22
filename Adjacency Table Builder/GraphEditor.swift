43feb2ecef1cf6858f62b68893e35f67c445ca00
dylanconklin <dylan.conklin@tutanota.com>
2023-09-22 00:05:43 -0700

    Comment Out Unused GraphEditor
    Commenting this code out because it is not currently being used, but may be added back in later.

--- Adjacency Table Builder/GraphEditor.swift
+++ Adjacency Table Builder/GraphEditor.swift
@@ -5,37 +5,37 @@
 //  Created by Dylan Conklin on 8/27/23.
 //
 
-import SwiftUI
-
-struct GraphEditor: View {
-    @Binding var graph: Graph
-    @State var graphElement: GraphElements = .edges
-    
-    enum GraphElements {
-        case edges
-        case vertices
-    }
-    
-    var body: some View {
-        VStack {
-            Picker("Type of graph to display", selection: $graphElement) {
-                Text("Edges").tag(GraphElements.edges)
-                Text("Vertices").tag(GraphElements.vertices)
-            }
-            .pickerStyle(.segmented)
-            .padding()
-            
-            switch graphElement {
-            case .edges:
-                EdgeList(graph: $graph)
-            case .vertices:
-                VertexList(graph: $graph)
-            }
-        }
-    }
-}
-
-#Preview {
-    @State var graph = Graph()
-    return GraphEditor(graph: $graph)
-}
+//import SwiftUI
+//
+//struct GraphEditor: View {
+//    @Binding var graph: Graph
+//    @State var graphElement: GraphElements = .edges
+//    
+//    enum GraphElements {
+//        case edges
+//        case vertices
+//    }
+//    
+//    var body: some View {
+//        VStack {
+//            Picker("Type of graph to display", selection: $graphElement) {
+//                Text("Edges").tag(GraphElements.edges)
+//                Text("Vertices").tag(GraphElements.vertices)
+//            }
+//            .pickerStyle(.segmented)
+//            .padding()
+//            
+//            switch graphElement {
+//            case .edges:
+//                EdgeList(graph: $graph)
+//            case .vertices:
+//                VertexList(graph: $graph)
+//            }
+//        }
+//    }
+//}
+//
+//#Preview {
+//    @State var graph = Graph()
+//    return GraphEditor(graph: $graph)
+//}
