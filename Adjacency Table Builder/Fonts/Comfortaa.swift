3d590cf866fd178fbbc643cb109f0e639fa2f36f
dylanconklin <dylan.conklin@tutanota.com>
2023-09-22 19:35:27 -0700

    Add Custom Font

---
+++ Adjacency Table Builder/Fonts/Comfortaa.swift
@@ -0,0 +1,12 @@
+//
+//  Comfortaa.swift
+//  Adjacency Table Builder
+//
+//  Created by Dylan Conklin on 9/22/23.
+//
+
+import SwiftUI
+
+struct Comfortaa {
+    static let body = Font.custom("Comfortaa-Regular", size: 24.0)
+}
