b29610c813f35ea6f5eddafa5b045f5c00ecd6a1
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2023-11-22 12:59:46 -0800

    Slightly Modified GraphFacts View

--- Adjacency Table Builder/GraphFacts.swift
+++ Adjacency Table Builder/GraphFacts.swift
@@ -28,26 +28,23 @@ struct GraphFacts: View {
 
     var body: some View {
         TabView {
+            GraphFact(fact: "The graph has \(graph.edges.count) edge\(graph.edges.count == 1 ? "" : "s") and \(graph.vertices.count) \(graph.vertices.count == 1 ? "vertex" : "vertices")",
+                      symbol: "point.topleft.down.curvedto.point.bottomright.up")
+                .tabItem {}
             GraphFact(fact: "The cost is \(graph.cost)",
                       symbol: "clock")
                 .tabItem {}
             GraphFact(fact: "The graph is \(graph.isCyclic ? "" : "a")cyclic",
                       symbol: graph.isCyclic ? "arrow.triangle.2.circlepath" : "exclamationmark.arrow.triangle.2.circlepath")
                 .tabItem {}
-            GraphFact(fact: "The graph has \(graph.vertices.count) \(graph.vertices.count == 1 ? "vertex" : "vertices")",
-                      symbol: "smallcircle.circle")
-                .tabItem {}
-            GraphFact(fact: "The graph has \(graph.edges.count) edge\(graph.edges.count == 1 ? "" : "s")",
-                      symbol: "point.topleft.down.curvedto.point.bottomright.up")
-                .tabItem {}
             GraphFact(fact: "The graph is \(graph.isConnected ? "" : "not ")connected",
                       symbol: "point.3.connected.trianglepath.dotted")
                 .tabItem {}
-            GraphFact(fact: "The graph is \(graph.isBipartite ? "" : "not ")bipartite",
-                      symbol: graph.isBipartite ? "rectangle.split.2x1" : "rectangle.split.2x1.slash")
-                .tabItem {}
             GraphFact(fact: "The graph is \(graph.isTree ? "" : "not ")a tree",
                       symbol: "tree")
+                .tabItem {}
+            GraphFact(fact: "The graph is \(graph.isBipartite ? "" : "not ")bipartite",
+                      symbol: graph.isBipartite ? "rectangle.split.2x1" : "rectangle.split.2x1.slash")
                 .tabItem {}
         }
         .tabViewStyle(.page(indexDisplayMode: .always))
