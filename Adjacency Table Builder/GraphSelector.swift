6a17d23f6c5c1d18e66b3e1640643f315206c731
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-02-15 20:27:55 -0800

    Added Deletion and Graph Naming Features to Graph Selector

--- Adjacency Table Builder/GraphSelector.swift
+++ Adjacency Table Builder/GraphSelector.swift
@@ -11,24 +11,60 @@ import SwiftUI
 struct GraphSelector: View {
     @Environment(\.modelContext) var modelContext
     @Query(sort: \Graph.lastAccessed, order: .reverse) var savedGraphs: [Graph]
+    @State var showGraphNamer: Bool = false
+    @State var graphName: String = ""
+    @Environment(\.dismiss) var dismiss
+
+    var df:  DateFormatter {
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .short
+        return df
+    }
+
+    func deleteGraph(at offsets: IndexSet) {
+        for index in offsets {
+            modelContext.delete(savedGraphs[index])
+        }
+        try! modelContext.save()
+        if savedGraphs.isEmpty {
+            modelContext.insert(Graph())
+        }
+    }
 
     var body: some View {
         NavigationStack {
             List {
                 ForEach(savedGraphs) { graph in
                     VStack {
-                        Text("Last Accessed: \(graph.lastAccessed)")
+                        Text(graph.name ?? "Unnamed Graph")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text("Last Accessed: \(df.string(from: graph.lastAccessed))")
+                            .frame(maxWidth: .infinity, alignment: .leading)
                     }
                     .onTapGesture {
                         graph.lastAccessed = Date.now
+                        modelContext.processPendingChanges()
                     }
                 }
+                .onDelete(perform: deleteGraph)
             }
             .toolbar {
                 Button {
-                    modelContext.insert(Graph())
+                    graphName = ""
+                    showGraphNamer = true
                 } label: {
                     Label("New Graph", systemImage: "plus")
+                }
+                .alert("Graph Name", isPresented: $showGraphNamer) {
+                    TextField("Graph Name", text: $graphName, prompt: Text("Graph Name"))
+                    Button ("Add") {
+                        graphName = graphName.trimmingCharacters(in: .whitespacesAndNewlines)
+                        graphName = graphName.isEmpty ? "Unnamed Graph" : graphName
+                        modelContext.insert(Graph(name: graphName))
+                        modelContext.processPendingChanges()
+                        dismiss()
+                    }
                 }
             }
             .navigationTitle("Saved Graphs")
