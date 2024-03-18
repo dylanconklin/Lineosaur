e9beed8d966d8de450689b1495a45b653fb2b353
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-03-18 11:43:01 -0700

    Add Personal Attribute to Setting Footer

--- Lineosaur/Settings.swift
+++ Lineosaur/Settings.swift
@@ -25,38 +25,35 @@ struct Settings: View {
     var body: some View {
         NavigationStack {
             Form {
-                Toggle("Show Edge Weights", isOn: $displayEdgeWeights)
+                Section {
+                    Toggle("Show Edge Weights", isOn: $displayEdgeWeights)
 
-                #warning("Unimplemented: Delete Detatched Vertices")
-                Toggle("Delete Detached Vertices", isOn: $deleteDetachedVertices)
+#warning("Unimplemented: Delete Detatched Vertices")
+                    Toggle("Delete Detached Vertices", isOn: $deleteDetachedVertices)
 
-                Picker("Compiler", selection: $compiler) {
-                    Text("Dot").tag(Compiler.dot)
-                    Text("FDP").tag(Compiler.fdp)
-                    Text("Neato").tag(Compiler.neato)
-                    Text("Circo").tag(Compiler.circo)
-                    Text("TwoPi").tag(Compiler.twopi)
-                    Text("Osage").tag(Compiler.osage)
-                    Text("Patchwork").tag(Compiler.patchwork)
-                }
-
-                NavigationLink {
-                    GraphSelector()
-                } label: {
-                    Label("Open Graph", systemImage: "folder")
-                }
-
-                NavigationLink {
-                    VStack {
-                        Text("Developed by Dylan Conklin")
-                            .padding()
-                        Text("Made with ❤️ in Portland, OR")
-                            .padding()
-                        Text("MIT License")
-                            .padding()
+                    Picker("Compiler", selection: $compiler) {
+                        Text("Dot").tag(Compiler.dot)
+                        Text("FDP").tag(Compiler.fdp)
+                        Text("Neato").tag(Compiler.neato)
+                        Text("Circo").tag(Compiler.circo)
+                        Text("TwoPi").tag(Compiler.twopi)
+                        Text("Osage").tag(Compiler.osage)
+                        Text("Patchwork").tag(Compiler.patchwork)
                     }
-                } label: {
-                    Label("About GraphApp", systemImage: "info")
+
+                    NavigationLink {
+                        GraphSelector()
+                    } label: {
+                        Label("Open Graph", systemImage: "folder")
+                    }
+                } footer: {
+                    VStack {
+                        Text("Made with ❤️ in Portland, OR")
+                        Text("MIT License")
+                    }
+                    .font(.callout)
+                    .padding()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
                 }
             }
             .navigationTitle("Settings")
