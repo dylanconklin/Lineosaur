06a8721c67dd282e1f43cbb01db901fa4dba33eb
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-03-20 00:04:51 -0700

    Fixed MST Test

--- LineosaurTests/Graph Tests/Weight Graph Tests/Weighted Graph Tests.swift
+++ LineosaurTests/Graph Tests/Weight Graph Tests/Weighted Graph Tests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import Lineosaur
 
 final class Weighted_Graph_Tests: XCTestCase {
-    
+
     var sut: Graph!
 
     override func setUp() {
@@ -22,22 +22,23 @@ final class Weighted_Graph_Tests: XCTestCase {
 
     func testMST() {
         // given
-        var expectedResult: Graph = Graph()
-        expectedResult.insert(Edge(from: "a", to: "b", weight: 1))
-        expectedResult.insert(Edge(from: "b", to: "c", weight: 2))
-        expectedResult.insert(Edge(from: "a", to: "d", weight: 22))
-        expectedResult.insert(Edge(from: "d", to: "f", weight: 11))
-        expectedResult.insert(Edge(from: "d", to: "g", weight: 16))
-        expectedResult.insert(Edge(from: "e", to: "g", weight: 6))
-        expectedResult.insert(Edge(from: "f", to: "k", weight: 28))
-        expectedResult.insert(Edge(from: "j", to: "k", weight: 5))
-        expectedResult.insert(Edge(from: "i", to: "j", weight: 13))
-        expectedResult.insert(Edge(from: "h", to: "i", weight: 8))
+        let expectedResult: Graph = Graph(graphEdges: [
+            Edge(from: "a", to: "b", weight: 1),
+            Edge(from: "b", to: "c", weight: 2),
+            Edge(from: "a", to: "d", weight: 22),
+            Edge(from: "d", to: "f", weight: 11),
+            Edge(from: "d", to: "g", weight: 16),
+            Edge(from: "e", to: "g", weight: 6),
+            Edge(from: "f", to: "k", weight: 28),
+            Edge(from: "j", to: "k", weight: 5),
+            Edge(from: "i", to: "j", weight: 13),
+            Edge(from: "h", to: "i", weight: 8),
+        ])
 
         // when
         let actualResult: Graph = sut.mst
 
         // then
-        XCTAssertEqual(actualResult, expectedResult)
+        XCTAssertEqual(actualResult.edges, expectedResult.edges)
     }
 }
