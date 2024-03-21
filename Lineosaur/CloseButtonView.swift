3744cc7990d305f4d26f2b155de8438b6301a9d3
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-03-21 01:31:15 -0700

    Add Navigation Title to CloseButtonView

--- Lineosaur/CloseButtonView.swift
+++ Lineosaur/CloseButtonView.swift
@@ -8,29 +8,33 @@
 import SwiftUI
 
 struct CloseButtonView<Content: View>: View {
-    @Environment(\.dismiss) var dismiss
+    @State var title: String?
     @ViewBuilder var content: () -> Content
+    @Environment(\.dismiss) var dismiss
 
     var body: some View {
-        VStack {
-            Button {
-                dismiss()
-            } label: {
-                Image(systemName: "xmark.circle.fill")
-                    .resizable()
-                    .frame(width: 20, height: 20)
-                    .padding()
-            }
-            .foregroundStyle(.primary)
-            .frame(maxWidth: .infinity, alignment: .topTrailing)
+        NavigationStack {
             content()
+                .navigationTitle(title == nil ? "" : title!)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                .toolbar {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .padding()
+                    }
+                    .foregroundStyle(.primary)
+                    .frame(maxWidth: .infinity, alignment: .topTrailing)
+                }
         }
     }
 }
 
 #Preview {
-    CloseButtonView() {
+    CloseButtonView(title: "Preview") {
         Spacer()
     }
 }
