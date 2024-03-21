e4a52e8ccaefcdbacd51929071ceae204172fb86
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-03-21 01:31:49 -0700

    Make GraphFacts a List Instead of Pages

--- Lineosaur/GraphFacts.swift
+++ Lineosaur/GraphFacts.swift
@@ -11,15 +11,25 @@ import SwiftUI
 struct GraphFact: View {
     var fact: String
     var symbol: String
+    var value: Bool?
 
     var body: some View {
-        VStack {
-            Spacer()
-            Image(systemName: symbol)
-                .font(.system(size: 64))
-            Spacer()
-            Text(fact)
-            Spacer()
+        Grid {
+            GridRow {
+                ZStack {
+                    Image(systemName: symbol)
+                        .font(.system(size: 32))
+                    if value == true {
+                        Image(systemName: "xmark")
+                            .font(.system(size: 48))
+                            .foregroundStyle(Color.red)
+                    }
+                }
+                .frame(minHeight: 48)
+                .padding(.horizontal)
+                Text(fact)
+                    .padding(.horizontal)
+            }
         }
     }
 }
@@ -28,29 +38,24 @@ struct GraphFacts: View {
     @Bindable var graph: Graph
     
     var body: some View {
-        VStack {
-            CloseButtonView() {
-                TabView {
-                    GraphFact(fact: "The graph has \(graph.edges.count) edge\(graph.edges.count == 1 ? "" : "s") and \(graph.vertices.count) \(graph.vertices.count == 1 ? "vertex" : "vertices")",
-                              symbol: "point.topleft.down.curvedto.point.bottomright.up")
-                    .tabItem {}
-                    GraphFact(fact: "The cost is \(graph.cost)",
-                              symbol: "clock")
-                    .tabItem {}
-                    GraphFact(fact: "The graph is \(graph.isCyclic ? "" : "a")cyclic",
-                              symbol: graph.isCyclic ? "arrow.triangle.2.circlepath" : "exclamationmark.arrow.triangle.2.circlepath")
-                    .tabItem {}
-                    GraphFact(fact: "The graph is \(graph.isConnected ? "" : "not ")connected",
-                              symbol: "point.3.connected.trianglepath.dotted")
-                    .tabItem {}
-                    GraphFact(fact: "The graph is \(graph.isTree ? "" : "not ")a tree",
-                              symbol: "tree")
-                    .tabItem {}
-                    GraphFact(fact: "The graph is \(graph.isBipartite ? "" : "not ")bipartite",
-                              symbol: graph.isBipartite ? "rectangle.split.2x1" : "rectangle.split.2x1.slash")
-                    .tabItem {}
-                }
-                .tabViewStyle(.page(indexDisplayMode: .always))
+        CloseButtonView(title: "Graph Facts") {
+            List {
+                GraphFact(fact: "The graph has \(graph.edges.count) edge\(graph.edges.count == 1 ? "" : "s") and \(graph.vertices.count) \(graph.vertices.count == 1 ? "vertex" : "vertices")",
+                          symbol: "point.topleft.down.curvedto.point.bottomright.up")
+                GraphFact(fact: "The cost is \(graph.cost)",
+                          symbol: "clock")
+                GraphFact(fact: "The graph is \(graph.isCyclic ? "" : "a")cyclic",
+                          symbol: "arrow.triangle.2.circlepath",
+                          value: !graph.isCyclic)
+                GraphFact(fact: "The graph is \(graph.isConnected ? "" : "not ")connected",
+                          symbol: "point.3.connected.trianglepath.dotted",
+                          value: !graph.isConnected)
+                GraphFact(fact: "The graph is \(graph.isTree ? "" : "not ")a tree",
+                          symbol: "tree",
+                          value: !graph.isTree)
+                GraphFact(fact: "The graph is \(graph.isBipartite ? "" : "not ")bipartite",
+                          symbol: "rectangle.split.2x1",
+                          value: !graph.isBipartite)
             }
         }
     }
