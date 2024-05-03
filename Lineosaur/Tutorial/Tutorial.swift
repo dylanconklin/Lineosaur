d41df004e3bb2397f282edd064756bea4c26f6f2
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-05-03 00:27:08 -0700

    Fixed Supporting Type Violation

--- Lineosaur/Tutorial/Tutorial.swift
+++ Lineosaur/Tutorial/Tutorial.swift
@@ -45,77 +45,77 @@ internal struct Tutorial: View {
             }
         }
     }
-}
 
-private struct Tutorial1: View {
-    var body: some View {
-        HStack {
-            Image(systemName: "app.connected.to.app.below.fill")
-                .accessibilityLabel("Tutorial label for adding edge")
-                .font(.system(size: 32))
-            Text("Click the + button to add edges to the graph\n\nStart by adding a single edge. "
-                + "The vertices will be added for you automatically!")
+    private struct Tutorial1: View {
+        var body: some View {
+            HStack {
+                Image(systemName: "app.connected.to.app.below.fill")
+                    .accessibilityLabel("Tutorial label for adding edge")
+                    .font(.system(size: 32))
+                Text("Click the + button to add edges to the graph\n\nStart by adding a single edge. "
+                     + "The vertices will be added for you automatically!")
                 .padding(.horizontal)
+            }
         }
     }
-}
 
-private struct Tutorial2: View {
-    var body: some View {
-        HStack {
-            Image(systemName: "character.bubble")
-                .accessibilityLabel("Tutorial label for naming vertices")
-                .font(.system(size: 32))
-            Text("The names of the endpoints can include spaces, special characters and emojis")
-                .padding(.horizontal)
+    private struct Tutorial2: View {
+        var body: some View {
+            HStack {
+                Image(systemName: "character.bubble")
+                    .accessibilityLabel("Tutorial label for naming vertices")
+                    .font(.system(size: 32))
+                Text("The names of the endpoints can include spaces, special characters and emojis")
+                    .padding(.horizontal)
+            }
         }
     }
-}
 
-private struct Tutorial3: View {
-    var body: some View {
-        HStack {
-            Image(systemName: "list.bullet")
-                .accessibilityLabel("Tutorial label for viewing edges")
-                .font(.system(size: 32))
-            Text("You will see the edges appear in a list as you add them")
-                .padding(.horizontal)
+    private struct Tutorial3: View {
+        var body: some View {
+            HStack {
+                Image(systemName: "list.bullet")
+                    .accessibilityLabel("Tutorial label for viewing edges")
+                    .font(.system(size: 32))
+                Text("You will see the edges appear in a list as you add them")
+                    .padding(.horizontal)
+            }
         }
     }
-}
 
-private struct Tutorial4: View {
-    var body: some View {
-        HStack {
-            Image(systemName: "point.3.filled.connected.trianglepath.dotted")
-                .accessibilityLabel("Tutorial label for view tabs")
-                .font(.system(size: 32))
-            Text("Tap the bottom of your screen to view the graph visually, or as a table")
-                .padding(.horizontal)
+    private struct Tutorial4: View {
+        var body: some View {
+            HStack {
+                Image(systemName: "point.3.filled.connected.trianglepath.dotted")
+                    .accessibilityLabel("Tutorial label for view tabs")
+                    .font(.system(size: 32))
+                Text("Tap the bottom of your screen to view the graph visually, or as a table")
+                    .padding(.horizontal)
+            }
         }
     }
-}
 
-private struct Tutorial5: View {
-    var body: some View {
-        HStack {
-            Image(systemName: "hand.tap.fill")
-                .accessibilityLabel("Tutorial label for gestures")
-                .font(.system(size: 32))
-            Text("Tap and hold the edges to see advanced actions")
-                .padding(.horizontal)
+    private struct Tutorial5: View {
+        var body: some View {
+            HStack {
+                Image(systemName: "hand.tap.fill")
+                    .accessibilityLabel("Tutorial label for gestures")
+                    .font(.system(size: 32))
+                Text("Tap and hold the edges to see advanced actions")
+                    .padding(.horizontal)
+            }
         }
     }
-}
 
-private struct Tutorial6: View {
-    var body: some View {
-        HStack {
-            Image(systemName: "square.and.arrow.down")
-                .accessibilityLabel("Tutorial label for loading and saving graphs")
-                .font(.system(size: 32))
-            Text("Create and load multiple graphs in Settings")
-                .padding(.horizontal)
+    private struct Tutorial6: View {
+        var body: some View {
+            HStack {
+                Image(systemName: "square.and.arrow.down")
+                    .accessibilityLabel("Tutorial label for loading and saving graphs")
+                    .font(.system(size: 32))
+                Text("Create and load multiple graphs in Settings")
+                    .padding(.horizontal)
+            }
         }
     }
 }
