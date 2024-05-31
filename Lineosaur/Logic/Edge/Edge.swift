989f1a1100d8edf934ca013f6cd940ae63dde289
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-05-31 00:21:40 -0700

    Removed Unused Functions in Edge

--- Lineosaur/Logic/Edge/Edge.swift
+++ Lineosaur/Logic/Edge/Edge.swift
@@ -22,29 +22,11 @@ internal struct Edge: Codable, Comparable, CustomStringConvertible, Hashable {
         [from, toward]
     }
 
-    internal var copy: Self {
-        Self(from: from, toward: toward, weight: weight)
-    }
-
     internal static func == (lhs: Self, rhs: Self) -> Bool {
         lhs.from == rhs.from && lhs.toward == rhs.toward && lhs.weight == rhs.weight
     }
 
     internal static func < (lhs: Self, rhs: Self) -> Bool {
         lhs.weight < rhs.weight
-    }
-
-    internal static func alphabetical() -> (Self, Self) -> Bool {
-        { (lhs: Self, rhs: Self) -> Bool in
-            var result: Bool = .init(false)
-            if lhs.from != rhs.from {
-                result = lhs.from < rhs.from
-            } else if lhs.toward != rhs.toward {
-                result = lhs.toward < rhs.toward
-            } else {
-                result = lhs.weight < rhs.weight
-            }
-            return result
-        }
     }
 }
