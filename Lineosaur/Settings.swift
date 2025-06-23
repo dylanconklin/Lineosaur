0c9792f7d76c8190c5da2e755972285da43591a7
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-06-23 00:49:43 -0700

    Removed NavigationStack in Settings

--- Lineosaur/Settings.swift
+++ Lineosaur/Settings.swift
@@ -9,19 +9,17 @@ import SwiftUI
 
 struct Settings: View {
     var body: some View {
-        NavigationStack {
+        VStack {
             VStack {
-                VStack {
-                    Text("Made with ❤️ in Portland, OR")
-                    Text("MIT License")
-                }
-                .font(.callout)
-                .padding()
+                Text("Made with ❤️ in Portland, OR")
+                Text("MIT License")
+            }
+            .font(.callout)
+            .padding()
 
-                if let url: URL = .init(string: "https://dylanconklin.github.io") {
-                    Link("Check out my other projects", destination: url)
-                        .navigationTitle("About")
-                }
+            if let url: URL = .init(string: "https://dylanconklin.github.io") {
+                Link("Check out my other projects", destination: url)
+                    .navigationTitle("About")
             }
         }
     }
